01a30ef9cabdb45dbc310d92ff81e87e5dae1de1
Dave Syer <dsyer@vmware.com>
2024-01-01 15:19:00 +0000

    Add devtools

---
+++ src/test/java/example/todomvc/web/TestApplication.java
@@ -0,0 +1,11 @@
+package example.todomvc.web;
+
+import example.todomvc.Application;
+
+public class TestApplication {
+
+	public static void main(String[] args) throws Exception {
+		Application.main(args);
+	}
+	
+}
